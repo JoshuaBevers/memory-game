7da9f19572ab0efaaed578123bfa1a9c02f86691
infernezor@gmail.com <infernezor@gmail.com>
2020-03-24 11:41:39 -0400

    Part 1.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
 
+//components
+import MemoryCard from "./components/MemoryCard";
+
 function App() {
   return (
     <div className="App">
@@ -8,6 +11,7 @@ function App() {
         <h1>Memory game</h1>
         <p>Match cards to win</p>
       </header>
+      <MemoryCard />
     </div>
   );
 }
---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./MemoryCard.css";
+
+class MemoryCard extends Component {
+  render() {
+    return <div className="MemoryCard">Hello</div>;
+  }
+}
+
+export default MemoryCard;
