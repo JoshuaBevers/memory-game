ff0278ac7d9bd95d6a4fcd9b5c5ec8f048146bee
infernezor@gmail.com <infernezor@gmail.com>
2020-03-25 10:05:19 -0400

    Completed step 4 of part 2.

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -3,16 +3,23 @@ import logo from "./digitalcrafts.png";
 import "./MemoryCard.css";
 
 class MemoryCard extends Component {
+  constructor() {
+    super();
+    this.state = { isFlipped: false };
+  }
+
+  clickHandler() {
+    this.setState({ isFlipped: !this.state.isFlipped });
+  }
+
   render() {
     return (
-      <div className="MemoryCard">
-        <div className="MemoryCardInner">
-          <div className="MemoryCardFront">
-            ∆
-            <div className="MemoryCardBack">
-              <img src={logo} alt="" className="Image"></img>
-            </div>
+      <div className="MemoryCard" onClick={this.clickHandler}>
+        <div className="MemoryCardInner flipped">
+          <div className="MemoryCardBack">
+            <img src={logo} alt="" className="Image"></img>
           </div>
+          <div className="MemoryCardFront">∆</div>
         </div>
       </div>
     );
